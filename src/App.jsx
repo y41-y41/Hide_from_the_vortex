3f63c1bf964d87f7deb7a61cc8e1b9c2a7d11c4d
y41-y41 <yazin7647@gmail.com>
2026-05-26 23:27:27 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import MainDashboard from './pages/MainDashboard.jsx'
 import Setup from './pages/Setup.jsx'
 import SOSPage from './pages/SOSPage.jsx'
 
 export default function App() {
   return (
-    <BrowserRouter basename="/Hide_from_the_vortex">
+    <HashRouter>
       <Routes>
         <Route path="/"          element={<MainDashboard />} />
         <Route path="/profile"   element={<Setup />} />
         <Route path="/sos"       element={<SOSPage />} />
         <Route path="*"          element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Hide_from_the_vortex/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'docs'
