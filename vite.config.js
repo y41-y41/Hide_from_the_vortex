95f120ee40577ee89c155e41ea587533c1656191
y41-y41 <yazin7647@gmail.com>
2026-05-26 22:20:24 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Hide_from_the_vortex/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
